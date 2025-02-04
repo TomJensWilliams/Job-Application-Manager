7fe5145d7c727d03bbff6fd7874d3d88a2875232
Thomas Williams <tomjenswilliams@gmail.com>
2025-02-04 11:38:12 -0800

    Almost noon, February 4th

--- application/interface_setup_patch.py
+++ application/interface_setup_patch.py
@@ -3,7 +3,7 @@ from interface_functions import *
 
 # EXPLINATION OF THE PURPOSE OF THISE FILE
 
-def main(*, print_statements=False)
+def main(*, print_statements=False):
     all_databases = ["dictionaries", "jobs", "searches"]
     all_websites = ["glassdoor", "indeed", "linkedin", "monster", "ziprecruiter"]
 
